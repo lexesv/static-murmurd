ba07afee2f4313437bd4cd4a9285b375910f585f
Alex Vasilega <lexesv@gmail.com>
2017-03-02 01:26:19 +0200

    Update murmurd.sh

--- script/murmurd.sh
+++ script/murmurd.sh
@@ -16,6 +16,9 @@ sed -ri "s%opt/Ice-3.3%opt/3.6.3%g" src/murmur/murmur.pro
 
 sed -ri "s%opt/Ice-3.3%opt/3.6.3%g" src/murmur/murmur_ice/murmur_ice.pro
 
+#/opt/dbus/lib needed because I was not able to build a QT without it.
+#And when compiling Murmur, the QT need this library :-).
+
 /opt/qt/bin/qmake -recursive main.pro QMAKE_LFLAGS+="-L/opt/dbus/lib -L/opt/glib/lib" CONFIG+="static no-bonjour no-dbus no-client"
 
 make -j4 release
